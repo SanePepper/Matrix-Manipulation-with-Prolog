762417b8d5bd6bf74289d10c34065d745369605f
SanePepper <pwc0205@gmail.com>
2019-11-26 21:38:00 +0800

    change blend

--- ass3.pl
+++ ass3.pl
@@ -1,5 +1,5 @@
-list_get([L1|L],0,L1).
-list_get([L1|L],J,R) :- list_get(L,J1,R), J is J1+1.
+list_get([L1|_],0,L1).
+list_get([_|L],J,R) :- list_get(L,J1,R), J is J1+1.
 mat_get([M1|_],0,J,R) :- list_get(M1,J,R).
 mat_get([_|M],I,J,R) :- mat_get(M,I1,J,R), I is I1+1.
 
@@ -11,7 +11,7 @@ list_count([],0).
 list_count([_|L],S) :- list_count(L,S1), S is S1+1.
 mat_count([],0).
 mat_count([M1|M],S) :- list_count(M1,S1), mat_count(M,S2), S is S1+S2.
-mat_mean(M,R) :- mat_sum(M,S), mat_count(M,C), R is S/C, !.
+mat_mean(M,R) :- mat_sum(M,S), mat_count(M,C), R is S/C.
 
 head_tail([],[],[]).
 head_tail([H],H,[]).
@@ -22,9 +22,9 @@ mat_trans([[]|_],[]).
 mat_trans(M,R) :- R = [M1|R1], mat_translate_1st_col(M,M1,MS), mat_trans(MS,R1), !.
 
 list_blend([],[],_,[]).
-list_blend([A1|A], [B1|B], W, C) :- list_blend(A,B,W,C1), E is W*A1+(1-W)*B1, C = [E|C1].
+list_blend([A1|A], [B1|B], W, [C1|C]) :- list_blend(A,B,W,C), C1 is W*A1+(1-W)*B1.
 mat_blend([],[],_,[]).
-mat_blend([A1|A], [B1|B], W, C) :- list_blend(A1,B1,W,C1), mat_blend(A,B,W,C2), C = [C1|C2], !.
+mat_blend([A1|A], [B1|B], W, [C1|C]) :- list_blend(A1,B1,W,C1), mat_blend(A,B,W,C).
 
 mat_1st_col([],[],[]).
 mat_1st_col([[H|T]|M],C,R) :- mat_1st_col(M,C1,R1), C = [H|C1], R = [T|R1].
@@ -33,4 +33,4 @@ list_dot([A1|A],[B1|B],C) :- list_dot(A,B,C1), C is A1*B1+C1.
 list_dot_mat(_,[[]|_],[]).
 list_dot_mat(A,B,C) :- mat_1st_col(B,B1,BS), list_dot(A,B1,C1), list_dot_mat(A,BS,C2), C = [C1|C2].
 mat_dot([],_,[]).
-mat_dot([A1|A],B,C) :- list_dot_mat(A1,B,C1), mat_dot(A,B,C2), C = [C1|C2], !.
+mat_dot([A1|A],B,C) :- list_dot_mat(A1,B,C1), mat_dot(A,B,C2), C = [C1|C2].
